3fad122c75bb2b1a16bb9954f8030d412849f4c4
Vladimir Mirchev <vladimir@picasse.com>
2021-10-07 11:10:59 +0300

    Added binary test helper method to stringify long binary strings

--- tests/src/Test/Helpers/Binary.php
+++ tests/src/Test/Helpers/Binary.php
@@ -4,10 +4,27 @@ namespace Test\Helpers;
 
 trait Binary {
 
+  /**
+   * @return string
+   */
   protected function toStringStream() : string {
     return implode('', array_map('chr', func_get_args()));
   }
 
+  /**
+   * @param string $string
+   * @return string
+   */
+  protected function stringToStringStream(string $string) : string {
+    return implode('',
+      array_map('chr',
+      array_map(function ($value) { return intval($value, 16); }, str_split($string, 2)
+    )));
+  }
+
+  /**
+   * @return \Iterator
+   */
   protected function toArrayStream() : \Iterator {
     return new \ArrayIterator(array_map('chr', func_get_args()));
   }
