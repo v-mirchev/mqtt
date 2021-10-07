a02191d8dbf5d3a4a0544209768426a3579c2951
Vladimir Mirchev <vladimir@picasse.com>
2021-10-07 23:52:39 +0300

    Introduced class for byte binary operations

---
+++ src/Mqtt/Protocol/Binary/Byte.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Mqtt\Protocol\Binary;
+
+class Byte {
+
+  /**
+   * @var int
+   */
+  protected $value;
+
+  /**
+   * @param type $value
+   * @return $this
+   */
+  public function set($value) : \Mqtt\Protocol\Binary\Byte {
+    $this->value = is_int($value) ?
+      (0xFF & (int)$value) :
+      \ord($value);
+    return $this;
+  }
+
+  /**
+   * @return int
+   */
+  public function get(): int {
+    return 0xFF & $this->value;
+  }
+
+  /**
+   * @param int $bit
+   * @param type $bitValue
+   * @return void
+   */
+  public function setBit(int $bit, $bitValue) : \Mqtt\Protocol\Binary\Byte {
+    $this->value = $bitValue ? $this->get() | (1 << $bit) : $this->get() & ~(1 << $bit);
+    return $this;
+  }
+
+  /**
+   * @param int $bit
+   * @return int
+   */
+  public function getBit(int $bit) : int {
+    return (bool)($this->get() & (1 << $bit));
+  }
+
+  /**
+   * @param int $startBit
+   * @param int $endBit
+   * @return int
+   */
+  public function getSub(int $startBit, int $endBit) : int {
+    return (int)(($this->get() & ((1 << ($endBit + 1)) - 1)) >> $startBit);
+  }
+
+  /**
+   * @param int $bit
+   * @return int
+   */
+  public function setSub(int $startBit, int $endBit, int $value) : \Mqtt\Protocol\Binary\Byte {
+    $mask = (int)(((1 << ($endBit + 1)) - 1) >> $startBit) << $startBit;
+    $shiftedValue = (($value << $startBit) & $mask);
+    $this->value &= (~$mask);
+    $this->value |= $shiftedValue;
+    return $this;
+  }
+
+  /**
+   * @return string
+   */
+  public function __toString() : string {
+    return \chr($this->get());
+  }
+
+  public function __clone() {
+    $this->value = 0;
+  }
+
+}
---
+++ tests/src/Mqtt/Protocol/Binary/ByteTest.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace Mqtt\Protocol\Binary;
+
+class ByteTest extends \PHPUnit\Framework\TestCase {
+
+  /**
+   * @var Byte
+   */
+  protected $object;
+
+  protected function setUp() {
+    $this->object = new Byte();
+  }
+
+  public function testSetLimitsTo8bitSize() {
+    $this->object->set(0xFFFF);
+    $this->assertEquals(0xFF, $this->object->get());
+    $this->object->set(0xFF00);
+    $this->assertEquals(0x00, $this->object->get());
+  }
+
+  public function testSetFromChar() {
+    $this->object->set('A');
+    $this->assertEquals(0x41, $this->object->get());
+  }
+
+  public function testSetBit() {
+    for ($bit = 0; $bit < 8; $bit ++) {
+      $this->object->set(0x00);
+
+      $this->object->setBit($bit, true);
+      $this->assertEquals(pow(2, $bit), $this->object->get());
+    }
+
+    for ($bit = 0; $bit < 8; $bit ++) {
+      $this->object->set(0xFF);
+      $this->object->setBit($bit, true);
+      $this->assertEquals(0xFF, $this->object->get());
+    }
+  }
+
+  public function testUnsetBit() {
+    for ($bit = 0; $bit < 8; $bit ++) {
+      $this->object->set(0x00);
+      $this->object->setBit($bit, false);
+      $this->assertEquals(0x00, $this->object->get());
+    }
+    for ($bit = 0; $bit < 8; $bit ++) {
+      $this->object->set(0xFF);
+      $this->object->setBit($bit, false);
+
+      $this->assertEquals(0xFF - pow(2, $bit), $this->object->get());
+    }
+  }
+
+  public function testGetBit() {
+    for ($value = 0; $value < 256; $value ++) {
+      $this->object->set($value);
+      for ($bit = 0; $bit < 8; $bit ++) {
+        $binary = str_pad(decbin($this->object->get()), 8, 0, STR_PAD_LEFT);
+        $this->assertEquals(
+          intval(substr($binary, - $bit - 1, 1)),
+          $this->object->getBit($bit)
+        );
+      }
+    }
+  }
+
+  public function testGetSub() {
+    $this->object->set(bindec('1111 1111'));
+    $this->assertEquals(bindec('1111'), $this->object->getSub(0, 3));
+    $this->object->set(bindec('1111 1010'));
+    $this->assertEquals(bindec('1010'), $this->object->getSub(0, 3));
+
+    $this->object->set(bindec('0000 0000'));
+    $this->assertEquals(bindec('0000'), $this->object->getSub(2, 5));
+    $this->object->set(bindec('0000 0100'));
+    $this->assertEquals(bindec('0001'), $this->object->getSub(2, 5));
+    $this->object->set(bindec('0000 1100'));
+    $this->assertEquals(bindec('0011'), $this->object->getSub(2, 5));
+    $this->object->set(bindec('0001 1100'));
+    $this->assertEquals(bindec('0111'), $this->object->getSub(2, 5));
+    $this->object->set(bindec('0011 1100'));
+    $this->assertEquals(bindec('1111'), $this->object->getSub(2, 5));
+    $this->object->set(bindec('0111 1100'));
+    $this->assertEquals(bindec('1111'), $this->object->getSub(2, 5));
+    $this->object->set(bindec('1111 1100'));
+    $this->assertEquals(bindec('1111'), $this->object->getSub(2, 5));
+    $this->object->set(bindec('1111 1110'));
+    $this->assertEquals(bindec('1111'), $this->object->getSub(2, 5));
+    $this->object->set(bindec('1111 1111'));
+    $this->assertEquals(bindec('1111'), $this->object->getSub(2, 5));
+    $this->object->set(bindec('1111 1111'));
+    $this->assertEquals(bindec('1111'), $this->object->getSub(2, 5));
+    $this->object->set(bindec('0010 0000'));
+    $this->assertEquals(bindec('1000'), $this->object->getSub(2, 5));
+    $this->object->set(bindec('0011 0000'));
+    $this->assertEquals(bindec('1100'), $this->object->getSub(2, 5));
+    $this->object->set(bindec('0011 1000'));
+    $this->assertEquals(bindec('1110'), $this->object->getSub(2, 5));
+    $this->object->set(bindec('0011 1100'));
+    $this->assertEquals(bindec('1111'), $this->object->getSub(2, 5));
+    $this->object->set(bindec('0011 1110'));
+    $this->assertEquals(bindec('1111'), $this->object->getSub(2, 5));
+    $this->object->set(bindec('0011 1111'));
+    $this->assertEquals(bindec('1111'), $this->object->getSub(2, 5));
+  }
+
+  public function testSetSub() {
+    $this->object->set(bindec('0000 0000'))->setSub(0, 7, bindec('111'));
+    $this->assertEquals(bindec('0000 0111'), $this->object->get());
+    $this->object->set(bindec('0000 0000'))->setSub(1, 7, bindec('111'));
+    $this->assertEquals(bindec('0000 1110'), $this->object->get());
+    $this->object->set(bindec('0000 0000'))->setSub(2, 7, bindec('111'));
+    $this->assertEquals(bindec('0001 1100'), $this->object->get());
+    $this->object->set(bindec('0000 0000'))->setSub(3, 7, bindec('111'));
+    $this->assertEquals(bindec('0011 1000'), $this->object->get());
+    $this->object->set(bindec('0000 0000'))->setSub(4, 7, bindec('111'));
+    $this->assertEquals(bindec('0111 0000'), $this->object->get());
+    $this->object->set(bindec('0000 0000'))->setSub(5, 7, bindec('111'));
+    $this->assertEquals(bindec('1110 0000'), $this->object->get());
+    $this->object->set(bindec('0000 0000'))->setSub(6, 7, bindec('111'));
+    $this->assertEquals(bindec('1100 0000'), $this->object->get());
+    $this->object->set(bindec('0000 0000'))->setSub(7, 7, bindec('111'));
+    $this->assertEquals(bindec('1000 0000'), $this->object->get());
+    $this->object->set(bindec('0000 0000'))->setSub(8, 10, bindec('111'));
+    $this->assertEquals(bindec('0000 0000'), $this->object->get());
+
+    $this->object->set(bindec('1111 0000'))->setSub(0, 7, bindec('111'));
+    $this->assertEquals(bindec('0000 0111'), $this->object->get());
+    $this->object->set(bindec('1111 0000'))->setSub(1, 7, bindec('111'));
+    $this->assertEquals(bindec('0000 1110'), $this->object->get());
+    $this->object->set(bindec('1111 0000'))->setSub(2, 7, bindec('111'));
+    $this->assertEquals(bindec('0001 1100'), $this->object->get());
+    $this->object->set(bindec('1111 0000'))->setSub(3, 7, bindec('111'));
+    $this->assertEquals(bindec('0011 1000'), $this->object->get());
+    $this->object->set(bindec('1111 0000'))->setSub(4, 7, bindec('111'));
+    $this->assertEquals(bindec('0111 0000'), $this->object->get());
+    $this->object->set(bindec('1111 0000'))->setSub(5, 7, bindec('111'));
+    $this->assertEquals(bindec('1111 0000'), $this->object->get());
+    $this->object->set(bindec('1111 0000'))->setSub(6, 7, bindec('111'));
+    $this->assertEquals(bindec('1111 0000'), $this->object->get());
+    $this->object->set(bindec('1111 0000'))->setSub(7, 7, bindec('111'));
+    $this->assertEquals(bindec('1111 0000'), $this->object->get());
+    $this->object->set(bindec('1111 0000'))->setSub(8, 10, bindec('111'));
+    $this->assertEquals(bindec('1111 0000'), $this->object->get());
+
+    $this->object->set(bindec('1111 0000'))->setSub(0, 2, bindec('111'));
+    $this->assertEquals(bindec('1111 0111'), $this->object->get());
+    $this->object->set(bindec('1111 0000'))->setSub(1, 3, bindec('111'));
+    $this->assertEquals(bindec('1111 1110'), $this->object->get());
+    $this->object->set(bindec('1111 0000'))->setSub(2, 4, bindec('111'));
+    $this->assertEquals(bindec('1111 1100'), $this->object->get());
+    $this->object->set(bindec('1111 0000'))->setSub(3, 5, bindec('111'));
+    $this->assertEquals(bindec('1111 1000'), $this->object->get());
+    $this->object->set(bindec('1111 0000'))->setSub(4, 6, bindec('111'));
+    $this->assertEquals(bindec('1111 0000'), $this->object->get());
+    $this->object->set(bindec('1111 0000'))->setSub(5, 7, bindec('111'));
+    $this->assertEquals(bindec('1111 0000'), $this->object->get());
+    $this->object->set(bindec('1111 0000'))->setSub(6, 8, bindec('111'));
+    $this->assertEquals(bindec('1111 0000'), $this->object->get());
+    $this->object->set(bindec('1111 0000'))->setSub(7, 9, bindec('111'));
+    $this->assertEquals(bindec('1111 0000'), $this->object->get());
+    $this->object->set(bindec('1111 0000'))->setSub(8, 10, bindec('111'));
+    $this->assertEquals(bindec('1111 0000'), $this->object->get());
+
+  }
+
+  public function testStringable() {
+    $this->object->set(0x41);
+    $this->assertEquals('A', (string)$this->object);
+  }
+
+}
