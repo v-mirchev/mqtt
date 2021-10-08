ab7f4577ff1337e480345b2d98413667e88d2f94
Vladimir Mirchev <vladimir@picasse.com>
2021-10-08 17:59:22 +0300

    Introduced word (double byted) binary operations class

---
+++ src/Mqtt/Protocol/Binary/Word.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Mqtt\Protocol\Binary;
+
+class Word {
+
+  /**
+   * @var \Mqtt\Protocol\Binary\Byte
+   */
+  protected $lsb;
+
+  /**
+   * @var \Mqtt\Protocol\Binary\Byte
+   */
+  protected $msb;
+
+  public function __construct(\Mqtt\Protocol\Binary\Byte $byte) {
+    $this->lsb = clone $byte;
+    $this->msb = clone $byte;
+  }
+
+  /**
+   * @return Byte
+   */
+  public function getMsb() {
+    return $this->msb;
+  }
+
+  /**
+   * @return Byte
+   */
+  public function getLsb() {
+    return $this->lsb;
+  }
+
+  /**
+   * @param int $value
+   * @return \Mqtt\Protocol\Binary\Word
+   */
+  public function set(int $value) : Word {
+    $this->msb->set($value >> 8);
+    $this->lsb->set($value % 256);
+    return $this;
+  }
+
+  /**
+   * @param mixed $msb
+   * @param mixed $lsb
+   * @return \Mqtt\Protocol\Binary\Word
+   */
+  public function setBytes($msb, $lsb) : Word {
+    $this->msb->set($msb);
+    $this->lsb->set($lsb);
+    return $this;
+  }
+
+  /**
+   * @return int
+   */
+  public function get(): int {
+    return 0xFFFF & ($this->lsb->get() + ($this->msb->get() << 8));
+  }
+
+  /**
+   * @return string
+   */
+  public function __toString() : string {
+    return $this->msb . $this->lsb;
+  }
+
+  public function __clone() {
+    $this->msb = clone $this->msb;
+    $this->lsb = clone $this->lsb;
+  }
+
+}
---
+++ tests/src/Mqtt/Protocol/Binary/WordTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Mqtt\Protocol\Binary;
+
+class WordTest extends \PHPUnit\Framework\TestCase {
+
+  /**
+   * @var Byte
+   */
+  protected $object;
+
+  protected function setUp() {
+    $this->object = new Word(new Byte());
+  }
+
+  public function testSetLimitsTo16bitSize() {
+    $this->object->set(0xFFFFFFFF);
+    $this->assertEquals(0xFFFF, $this->object->get());
+    $this->object->set(0xFF0000);
+    $this->assertEquals(0x0000, $this->object->get());
+  }
+
+  public function testSetFromChar() {
+    $this->object->setBytes('A', 'B');
+    $this->assertEquals(0x4142, $this->object->get());
+  }
+
+  public function testStringable() {
+    $this->object->set(0x4142);
+    $this->assertEquals('AB', (string)$this->object);
+  }
+
+}
