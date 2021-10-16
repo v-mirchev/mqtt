6ef284c55c5072af620f88d9a71381b7c48d79ad
Vladimir Mirchev <vladimir@picasse.com>
2021-10-16 16:27:10 +0300

    Added byte/char buffer class

---
+++ src/Mqtt/Protocol/Binary/Buffer.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Mqtt\Protocol\Binary;
+
+class Buffer {
+
+  /**
+   * @var string
+   */
+  protected $buffer;
+
+  /**
+   * @var int
+   */
+  protected $position;
+
+  public function __construct() {
+    $this->buffer = '';
+    $this->position = 0;
+  }
+
+  public function __clone() {
+    $this->buffer = '';
+    $this->position = 0;
+  }
+
+  /**
+   * @param int $length
+   * @return string
+   */
+  public function getString(int $length = null): string {
+    if ($this->position + $length > strlen($this->buffer)) {
+      throw new \Exception('Trying to read outside buffer');
+    }
+    $value = is_null($length) ?
+       substr($this->buffer, $this->position) :
+       substr($this->buffer, $this->position, $length);
+    $this->position = $length ? $this->position + $length : strlen($this->buffer);
+    return $value;
+  }
+
+  /**
+   * @param int $count
+   * @return int[]
+   */
+  public function getBytes(int $count = null): array {
+    $value = $this->getString($count);
+    if ($value === '') {
+      return [];
+    }
+    return array_map('ord', str_split($value));
+  }
+
+  /**
+   * @return int
+   */
+  public function getByte(): int {
+    return ord($this->getString(1));
+  }
+
+  /**
+   * @return bool
+   */
+  public function eof(): bool {
+    return $this->position === strlen($this->buffer);
+  }
+
+  /**
+   * @param string $buffer
+   * @return $this
+   */
+  public function set(string $buffer) {
+    $this->buffer = $buffer;
+    $this->position = 0;
+    return $this;
+  }
+
+  /**
+   * @return string
+   */
+  public function __toString() {
+    return $this->getString();
+  }
+
+}
---
+++ tests/src/Mqtt/Protocol/Binary/BufferTest.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Mqtt\Protocol\Binary;
+
+class BufferTest extends \PHPUnit\Framework\TestCase {
+
+  /**
+   * @var Buffer
+   */
+  protected $object;
+
+  protected function setUp() {
+    $this->object = new Buffer();
+  }
+
+  public function testInitiallyEmpty() {
+    $this->assertEmpty($this->object->getString());
+  }
+
+  public function testCloneResets() {
+    $this->object->set('ABC');
+    $object = clone $this->object;
+    $this->assertEmpty($object->getString());
+  }
+
+  public function testInitiallyEoF() {
+    $this->assertTrue($this->object->eof());
+  }
+
+  public function testEoFAtfterBufferSetNotTrue() {
+    $this->object->set('ABC');
+    $this->assertFalse($this->object->eof());
+  }
+
+  public function testEoFAtfterBufferConsumedIsTrue() {
+    $this->object->set('ABC');
+    $this->object->getString();
+    $this->assertTrue($this->object->eof());
+  }
+
+  public function testThrowsExceptionWhenReadingStringOutsideBuffer() {
+    $this->object->set('ABC');
+    $this->expectException(\Exception::class);
+    $this->object->getString(4);
+  }
+
+  public function testThrowsExceptionWhenReadingBytesOutsideBuffer() {
+    $this->object->set('ABC');
+    $this->expectException(\Exception::class);
+    $this->object->getBytes(4);
+  }
+
+  public function testThrowsExceptionWhenReadingByteOutsideBuffer() {
+    $this->object->set('ABC');
+    $this->object->getBytes(3);
+
+    $this->expectException(\Exception::class);
+    $this->object->getByte();
+  }
+
+  public function testReadStringWithSpecifiedLengthProperly() {
+    $this->object->set('ABC');
+    $this->assertEquals('AB', $this->object->getString(2));
+  }
+
+  public function testReadingStringShiftsBuffer() {
+    $this->object->set('ABCDE');
+    $this->object->getString(2);
+    $this->assertEquals('CD', $this->object->getString(2));
+  }
+
+  public function testReadingStringWithoutLengthReturnsRemainingBuffer() {
+    $this->object->set('ABCDE');
+    $this->object->getString(2);
+    $this->assertEquals('CDE', $this->object->getString());
+  }
+
+  public function testAsStringRturnsRemainingBuffer() {
+    $this->object->set('ABCD');
+    $this->object->getString(2);
+    $this->assertEquals('CD', (string)$this->object);
+  }
+
+  public function testAsStringRturnsEmptiesBuffer() {
+    $this->object->set('ABCD');
+    (string)$this->object;
+    $this->assertEmpty($this->object->getString());
+  }
+
+  public function testReadBytesWithSpecfiedCountPropely() {
+    $this->object->set('ABCD');
+    $this->assertEquals([ 0x41, 0x42], $this->object->getBytes(2));
+  }
+
+  public function testReadBytesShiftsBuffer() {
+    $this->object->set('ABCDE');
+    $this->object->getBytes(2);
+    $this->assertEquals([ 0x43, 0x44], $this->object->getBytes(2));
+  }
+
+  public function testReadAsBytesReturnsRemainingBufferBytes() {
+    $this->object->set('ABCDE');
+    $this->object->getBytes(2);
+    $this->assertEquals([ 0x43, 0x44, 0x45], $this->object->getBytes());
+  }
+
+  public function testReadAsBytesEmptiesBufferBytes() {
+    $this->object->set('ABCDE');
+    $this->object->getBytes();
+    $this->assertEmpty($this->object->getBytes());
+  }
+
+  public function testReadByteReturnsProperlySingleByteFromBuffer() {
+    $this->object->set('ABCDE');
+    $this->assertEquals(0x41, $this->object->getByte());
+  }
+
+  public function testReadByteShiftsBuffer() {
+    $this->object->set('ABCDE');
+    $this->object->getByte();
+    $this->assertEquals(0x42, $this->object->getByte());
+  }
+
+}
